bf8b17859e55c3cd8fb858c54306b64dd196477a
Skyramp Eval <eval@skyramp.dev>
2026-08-23 11:31:51 -0700

    feat: add client-side validation on project name input in export dialogs

--- packages/excalidraw/components/ProjectName.tsx
+++ packages/excalidraw/components/ProjectName.tsx
@@ -7,6 +7,8 @@ import { useExcalidrawContainer } from "./App";
 import "./TextInput.scss";
 import "./ProjectName.scss";
 
+const ILLEGAL_CHARS = /[/\\:*?"<>|]/;
+
 type Props = {
   value: string;
   onChange: (value: string) => void;
@@ -17,8 +19,12 @@ type Props = {
 export const ProjectName = (props: Props) => {
   const { id } = useExcalidrawContainer();
   const [fileName, setFileName] = useState<string>(props.value);
+  const [touched, setTouched] = useState(false);
+
+  const isInvalid = touched && (!fileName || ILLEGAL_CHARS.test(fileName));
 
   const handleBlur = (event: any) => {
+    setTouched(true);
     if (!props.ignoreFocus) {
       focusNearestParent(event.target);
     }
@@ -51,7 +57,15 @@ export const ProjectName = (props: Props) => {
         id={`${id}-filename`}
         value={fileName}
         onChange={(event) => setFileName(event.target.value)}
+        aria-invalid={isInvalid ? "true" : undefined}
       />
+      {isInvalid && (
+        <span data-testid="project-name-error">
+          {!fileName
+            ? "Project name is required"
+            : "Project name contains invalid characters"}
+        </span>
+      )}
     </div>
   );
 };
